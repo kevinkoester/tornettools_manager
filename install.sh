3a3b68ceac943e9743977d1792e1fff424170849
Kevin Köster <kevin.koester@uni-hamburg.de>
2021-11-02 09:31:00 +0000

    Add support to install shadow 2 in install script

--- install.sh
+++ install.sh
@@ -5,15 +5,42 @@ SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 
 INSTALL_PREFIX=${SCRIPT_DIR}/bin
 
+TOR_COMMIT=main
+#SHADOW_COMMIT=v1.15.0
+SHADOW_COMMIT=v2.0.0
+TORNETTOOLS_COMMIT=mods2
+
+function get_shadow_version {
+	shadow --version 2>&1 | perl -lne '/Shadow v([\d]+)\.[\d]+/ && print $1'
+}
+
 function install_shadow {
 	pushd ${SCRIPT_DIR}/dependencies/shadow
-	./setup build --use-cpu-timer --prefix  ${INSTALL_PREFIX}
+	git checkout $SHADOW_COMMIT
+	if [ "$(get_shadow_version)" -ge 2 ]; then
+		./setup build --prefix  ${INSTALL_PREFIX}
+	else
+		./setup build --enable-cpu-timer --prefix  ${INSTALL_PREFIX}
+	fi
 	./setup install
+	popd
+}
+
+function install_tor {
+	pushd ${SCRIPT_DIR}/dependencies/tor
+	git checkout $TOR_COMMIT
+	./autogen.sh
+	./configure --disable-asciidoc --disable-unittests --disable-manpage --disable-html-manual --prefix ${INSTALL_PREFIX}
+	make -j$(nproc)
+	make install
 	popd
 }
 
 function install_shadow_plugin_tor {
 	pushd ${SCRIPT_DIR}/dependencies/shadow-plugin-tor
+	pushd ../tor
+	git checkout $TOR_COMMIT
+	popd
 	yes | ./setup dependencies
 	yes | ./setup build --tor-prefix ../tor --prefix  ${INSTALL_PREFIX} --shadow-root ${INSTALL_PREFIX}
 	./setup install
@@ -21,8 +48,8 @@ function install_shadow_plugin_tor {
 }
 
 function install_tgen {
-	mkdir dependencies/tgen/build
-	pushd dependencies/tgen/build
+	mkdir ${SCRIPT_DIR}/dependencies/tgen/build
+	pushd ${SCRIPT_DIR}/dependencies/tgen/build
 	cmake .. -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX}
 	make -j5
 	make install
@@ -30,14 +57,15 @@ function install_tgen {
 }
 
 function install_tornettools {
-	pushd dependencies/tornettools
+	pushd ${SCRIPT_DIR}/dependencies/tornettools
+	git checkout $TORNETTOOLS_COMMIT
 	pip install wheel
 	pip install -r requirements.txt
 	pip install -I .
 	popd
 }
 function install_oniontrace {
-	mkdir dependencies/oniontrace/build
+	mkdir ${SCRIPT_DIR}/dependencies/oniontrace/build
 	pushd dependencies/oniontrace/build
 	cmake .. -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX}
 	make -j5
@@ -46,32 +74,36 @@ function install_oniontrace {
 }
 
 function install_oniontracetools {
-	pushd dependencies/oniontrace/tools
+	pushd ${SCRIPT_DIR}/dependencies/oniontrace/tools
 	pip install -r requirements.txt
 	pip install -I .
 	popd
 }
 
 function install_tgentools {
-	pushd dependencies/tgen/tools
+	pushd ${SCRIPT_DIR}/dependencies/tgen/tools
 	pip install -r requirements.txt
 	pip install -I .
 	popd
 }
 
 
-git submodule update --init --recursive
+#git submodule update --init --recursive
 VENV_DIR=${SCRIPT_DIR}/venv
 if [ ! -d "${VENV_DIR}" ]; then
 	python3 -m venv ${VENV_DIR}
 fi
 
-source ${VENV_DIR}/bin/activate
+source ${SCRIPT_DIR}/activate_env.sh
 
 pip install pyelftools
 
 install_shadow
-install_shadow_plugin_tor
+if [ "$(get_shadow_version)" -ge 2 ]; then
+	install_tor
+else
+	install_shadow_plugin_tor
+fi
 install_tgen
 install_tgentools
 install_tornettools
